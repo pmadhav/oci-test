f3b0e3b9b07f70b1cf29d7e3e7306b4968f29949
Praveen Madhav <praveen.madhav@securiti.ai>
2021-05-25 10:04:36 -0700

    removed user_ocid, fingerprint & private_key_path variables.

--- provider.tf
+++ provider.tf
@@ -1,13 +1,7 @@
 variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
 variable "region" {}
 
 provider "oci" {
   region           = var.region
   tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
 }
